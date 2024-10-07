655257f0a1457002f8ccf95123d09f70ea3db3c6
Dhanish <dhanishmohammed814@gmail.com>
2024-10-07 22:30:46 +0530

    remove the codes

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-const nextConfig = {
-  // Enable React Strict Mode for highlighting potential issues in your app
-  reactStrictMode: true,
-
-  // Configure image optimization
-  images: {
-    // Add any external domains you're using for images
-    domains: ['dhanish.vercel.app'], // replace 'example.com' with the domain of your image hosting service
-  },
-}
+const nextConfig = {};
 
 export default nextConfig;
