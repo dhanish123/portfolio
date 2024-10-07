de908c1206367d79f79d24f6c30fb1d6198de92b
Dhanish <dhanishmohammed814@gmail.com>
2024-10-07 22:22:34 +0530

    update image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+const nextConfig = {
+    // Enable React Strict Mode for highlighting potential issues in your app
+    reactStrictMode: true,
+  
+    // Configure image optimization
+    images: {
+      // Add any external domains you're using for images
+      domains: ['example.com'], // replace 'example.com' with the domain of your image hosting service
+    },
+}  
 
 export default nextConfig;
