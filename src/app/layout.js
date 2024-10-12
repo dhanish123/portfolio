217c33844cc69331df3b0bde61652a59e9652ab7
Dhanish <dhanishmohammed814@gmail.com>
2024-10-12 11:23:33 +0530

    gtm fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,25 +39,21 @@ export default function RootLayout({ children }) {
         {/* <!-- Google Tag Manager --> */}
 
        {/* Google Tag Manager */}
-       <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M4J7SQ27');
-          `}
-        </Script>
+  {
+    `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M4J75QZ7');</script>`
+  }
         {/* <!-- End Google Tag Manager --> */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
                 {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4J7SQ27"
-            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
-        </noscript>
+                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4J75QZ7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
         {children}
       </body>
