89161097c6a96355f96fb192b7e69669aaea2191
Dhanish <dhanishmohammed814@gmail.com>
2024-10-14 22:00:47 +0530

    fixed

--- src/app/Components/AboutSection.jsx
+++ src/app/Components/AboutSection.jsx
@@ -13,6 +13,102 @@ import {
 } from "react-icons/fa";
 
 const TAB_DATA = [
+  // {
+  //   title: "Skills",
+  //   id: "skills",
+  //   content: (
+  //     <Marquee direction="right">
+  //       <div className="inline-block  animate-marquee">
+  //         <Image
+  //           src="/images/react.svg"
+  //           alt="React"
+  //           width={80}
+  //           height={80}
+  //           className="inline-block rounded mx-4"
+  //         />
+  //         <Image
+  //           src="/images/git.svg"
+  //           alt="Github"
+  //           width={80}
+  //           height={80}
+  //           className="inline-block rounded mx-4"
+  //         />
+  //         <Image
+  //           src="/images/GA4.png"
+  //           alt="Google Analytics 4"
+  //           width={180}
+  //           height={180}
+  //           className="inline-block rounded mx-4"
+  //         />
+  //         <Image
+  //           src="/images/html.svg"
+  //           alt="HTML"
+  //           width={80}
+  //           height={80}
+  //           className="inline-block rounded mx-4"
+  //         />
+  //         <Image
+  //           src="/images/sass.png"
+  //           alt="SCSS"
+  //           width={80}
+  //           height={80}
+  //           className="inline-block rounded mx-4"
+  //         />
+  //         <Image
+  //           src="/images/css.svg"
+  //           alt="CSS"
+  //           width={80}
+  //           height={80}
+  //           className="inline-block rounded mx-4"
+  //         />
+  //         <Image
+  //           src="/images/js.png"
+  //           alt="JavaScript"
+  //           width={100}
+  //           height={100}
+  //           className="inline-block rounded mx-4"
+  //         />
+  //         <Image
+  //           src="/images/bootstrap.svg"
+  //           alt="Bootstrap"
+  //           width={80}
+  //           height={80}
+  //           className="inline-block rounded mx-4"
+  //         />
+
+  //         <Image
+  //           src="/images/vitejs.svg"
+  //           alt="vitejs"
+  //           width={80}
+  //           height={80}
+  //           className="inline-block rounded mx-4"
+  //         />
+  //         <Image
+  //           src="/images/materialui.svg"
+  //           alt="materialui"
+  //           width={80}
+  //           height={80}
+  //           className="inline-block rounded mx-4"
+  //         />
+  //         <Image
+  //           src="/images/tailwind.svg"
+  //           alt="Tailwind CSS"
+  //           width={80}
+  //           height={80}
+  //           className="inline-block rounded mx-4"
+  //         />
+
+  //         <Image
+  //           src="/images/next.png"
+  //           alt="Next.js"
+  //           width={80}
+  //           height={80}
+  //           className="inline-block rounded mx-4"
+  //         />
+  //       </div>
+  //     </Marquee>
+  //   ),
+  // },
   {
     title: "Skills",
     id: "skills",
@@ -245,7 +341,7 @@ const AboutSection = () => {
     <section id="about" className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 md:px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
-          src="/Images/about-image.png"
+          src="/images/about-image.png"
           alt="About Me"
           width={500}
           height={500}
@@ -253,6 +349,8 @@ const AboutSection = () => {
         />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+                    {/* <h2 className="text-4xl font-bold text-white mb-4">Who am i?</h2> */}
+
           <p className="text-base lg:text-lg mb-[12px]">
             {/* I am a passionate React developer with [X years] of experience
             building dynamic, responsive web applications. My expertise lies in
@@ -260,8 +358,8 @@ const AboutSection = () => {
             ensuring high-quality code. I have a strong foundation in
             JavaScript, HTML, CSS, and have extensive experience with modern
             libraries like React.js, Redux, and Next.js. */}
-            I&apos;m a Front End Developer specializing in building visually
-            stunning and intuitive websites with React and other modern
+            I&apos;m a Front End Developer specializing in building visually stunning
+            and intuitive websites with React and other modern
             technologies. Experienced in using GA4 to track user behavior and
             enhance user privacy.
           </p>
@@ -322,7 +420,7 @@ const AboutSection = () => {
               selectTab={() => handleTabChange("certification")}
               active={tab === "certification"}
             >
-              Certification
+              Certifications
             </TabButton>
           </div>
           <div className="mt-8">
