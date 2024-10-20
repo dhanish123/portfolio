c099a8ceffe3d51d2ef4f0dcebb0b25e1b5db94a
Dhanish <dhanishmohammed814@gmail.com>
2024-10-20 11:57:15 +0530

    fix

--- src/app/Components/AboutSection.jsx
+++ src/app/Components/AboutSection.jsx
@@ -22,7 +22,7 @@ const TAB_DATA = [
           {/* Skills Progress Bars */}
           {[
             {
-              src: "/images/css.svg",
+              src: "/images/html.svg",
               alt: "HTML5",
               name: "HTML5",
               width: "w-[90%]",
