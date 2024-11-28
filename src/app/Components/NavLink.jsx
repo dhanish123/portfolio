eedbe7773cbce325b08c5d20cd82dd507b3961ae
Dhanish <dhanishmohammed814@gmail.com>
2024-11-28 23:12:09 +0530

    updated

--- src/app/Components/NavLink.jsx
+++ src/app/Components/NavLink.jsx
@@ -9,7 +9,7 @@
         //  hover:text-white hover:bg-[#1E1E1E] hover:border hover:border-[#00FFDD] hover:shadow-lg"
         // className="block py-1 px-[6px] text-[#ADB7BE] sm:text-xl rounded-md
         // hover:text-white hover:bg-[#1E1E1E] hover:border hover:border-[#00FFDD] hover:shadow-lg"
-        className="block py-1 px-2 md:py-0.5 md:px-1.5 text-[#ADB7BE] sm:text-xl rounded-md transition-all duration-300 
+        className="block py-2 px-3 md:py-0.5 md:px-1.5 text-[#ADB7BE] sm:text-xl rounded-md transition-all duration-300 
            border border-transparent hover:text-white hover:bg-[#1E1E1E] hover:border-[#00FFDD] hover:shadow-lg"
 
 
