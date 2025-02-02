ed795f7569bae6ce647e2021273a093f89df6bde
Dhanish <dhanishmohammed814@gmail.com>
2025-02-02 19:59:04 +0530

    update

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+
+module.exports = {
+    siteUrl: 'https://dhanish.vercel.app/', // Replace with your website's URL
+    generateRobotsTxt: true, // Optional: Generate robots.txt
+    // Additional options can be added here
+  }
+  
--- next-sitemap.js
+++ next-sitemap.js
@@ -1,8 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: 'https://dhanish.vercel.app', // Replace with your live URL
-    generateRobotsTxt: true, // Generate robots.txt file
-    outDir: 'public', // Ensure the sitemap is written to the public directory
+    siteUrl: 'https://dhanish.vercel.app', // Your live site URL
+    generateRobotsTxt: true, // Generates robots.txt
+    outDir: './public', // Ensure sitemap and robots.txt are outputted to the public folder
   };
   
   module.exports = config;
